f3f93ee143007d73fe7f4fab46a3118084a82e3a
asitos <4shut0shparashar@gmail.com>
2026-07-06 23:53:56 +0530

    feat(patterns): n*(n+1)/2 characters printed

---
+++ 01_patterns/18_alphaTrianglePattern.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+  void printPattern(int n) {
+    // my Solution
+    for (int i = 0; i < n; i++) {
+
+
+      for (char ch = ('A' + n - 1) - i; ch <= ('A' + n - 1); ch++) {
+
+        cout << ch << " ";
+      }
+      cout << "\n";
+    }
+  }
+};
+
+int main() {
+  Solution obj;
+  int n = 5;
+
+  obj.printPattern(n);
+  return 0;
+}
