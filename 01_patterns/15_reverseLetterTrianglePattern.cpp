5c1e52b353d96008e93108e716d7e42c0afecb0e
asitos <4shut0shparashar@gmail.com>
2026-07-06 22:22:00 +0530

    feat(patterns): O(N^2) reverse letter traingle pattern

---
+++ 01_patterns/15_reverseLetterTrianglePattern.cpp
@@ -0,0 +1,24 @@
+
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+  void printPattern(int n) {
+    // my Solution
+    for (int i = n - 1; i >= 0; i--) {
+      for (char ch = 'A'; ch <= 'A' + i; ch++) {
+        cout << ch << " ";
+      }
+      cout << "\n";
+    }
+  }
+};
+
+int main() {
+  Solution obj;
+  int n = 5;
+
+  obj.printPattern(n);
+  return 0;
+}
