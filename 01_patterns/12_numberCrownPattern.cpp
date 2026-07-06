9bfe00ed34cda45aa469748944fdb1a7ebd259d8
asitos <4shut0shparashar@gmail.com>
2026-07-06 21:54:59 +0530

    feat(patterns): O(N^2) crown pattern

--- 01_patterns/12_numberCrownPattern.cpp
+++ 01_patterns/12_numberCrownPattern.cpp
@@ -5,14 +5,14 @@ class Solution {
 public:
   void printPattern(int n) {
     // my Solution
-    for (int i = 0; i < n; i++) {
-      for (int j = 1; j < i + 1; j++) {
+    for (int i = 1; i <= n; i++) {
+      for (int j = 1; j <= i; j++) {
         cout << j;
       }
-      for (int j = 0; j < (n - 1 - i) * 2; j++) {
+      for (int j = 0; j < (n - i) * 2; j++) {
         cout << " ";
       }
-      for (int j = i; j > 0; j--) {
+      for (int j = i; j >= 1; j--) {
         cout << j;
       }
       cout << "\n";
