8e7466212cc1c220378d5cf39f4af74e52c6e89f
asitos <4shut0shparashar@gmail.com>
2026-07-06 16:16:44 +0530

    feat(patterns): O(N^2 pyramids

---
+++ 01_patterns/07-starPyramid.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+  void printPattern(int n) {
+    char c = '*';
+
+    for (int i = 0; i < n; i++) {
+
+      for (int j = 0; j < n - i - 1; j++) {
+        cout << " ";
+      }
+
+      for (int j = 0; j < (i * 2) + 1; j++) {
+        cout << c;
+      }
+
+      for (int j = 0; j < n - i - 1; j++) {
+        cout << " ";
+      }
+      
+      cout << "\n";
+    }
+  }
+  
+};
+
+int main () {
+  Solution obj;
+  int n = 5;
+  
+  obj.printPattern(n);  
+
+  return 0;
+}
+
