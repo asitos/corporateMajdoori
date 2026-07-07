a9feee73e0a1a2e67df2cdb31ad5b5a7c14041f3
asitos <4shut0shparashar@gmail.com>
2026-07-07 14:37:02 +0530

    feat(patterns): O(N^2) binary number triangle pattern

--- 01_patterns/11_binaryNumberTrianglePattern.cpp
+++ 01_patterns/11_binaryNumberTrianglePattern.cpp
@@ -1,14 +1,16 @@
-
 #include <bits/stdc++.h>
+
 using namespace std;
 
 void pattern11(int N) {
     int start = 1;
     
     for (int i = 0; i < N; i++) {
-        if (i % 2 == 0) start = 1;
-        
-        else start = 0;
+        if (i % 2 == 0) {
+          start = 1;
+        } else {
+          start = 0;
+        }
         
         for (int j = 0; j <= i; j++) {
             cout << start;
