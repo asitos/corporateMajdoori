6f93246cb590f0a9fbca2261c192a6504ea51870
asitos <4shut0shparashar@gmail.com>
2026-07-07 15:34:18 +0530

    script: update script

--- scripts/push.sh
+++ scripts/push.sh
@@ -15,17 +15,14 @@ fi
 
 DIR_NAME=$(dirname "$FILE")
 
-# 1. grab the real folder name, even if neovim just passes "."
 if [ "$DIR_NAME" == "." ]; then
     RAW_DIR=$(basename "$PWD")
 else
     RAW_DIR=$(basename "$DIR_NAME")
 fi
 
-# 2. strip the prefix and format it
 PATTERN=$(echo "$RAW_DIR" | sed 's/^[0-9]*_//' | tr '_' ' ' | tr '[:upper:]' '[:lower:]')
 
-# 3. only default to general if you are pushing from the absolute root of the repo
 if [ "$RAW_DIR" == "!corporateMajdoori" ]; then
     PATTERN="general"
 fi
