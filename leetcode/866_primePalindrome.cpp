616819ea8ca51385eb961aa67faf15bc0852cadc
asitos <4shut0shparashar@gmail.com>
2026-07-09 13:57:09 +0530

    feat(leetcode): O(p.sqrt(n); p=palindromes evaluated, short-circuit evalutation, number-theory (11-divisibility law

--- leetcode/866_primePalindrome.cpp
+++ leetcode/866_primePalindrome.cpp
@@ -4,7 +4,23 @@ using namespace std;
 class Solution {
 public:
   int primePalindrome(int n) {
-    
+    int i = n;
+
+    while(true) {
+      // 11-divisibility bypass
+      // skip 4-digit, 6-digit, and 8-digit nos.
+      // since they're all divisible by 11 if they're a palindrome of even length
+
+      if (1000 <= i && i < 10000) i = 10000;
+      if (100000 <= i && i < 1000000) i = 1000000;
+      if (100000 <= i && i < 100000000) i = 100000000;
+      if (isPalindrome(i) && isPrime(i)) {
+        return i;
+      }
+      i++;
+    }
+
+    return n;
   }
 
 private:
@@ -13,8 +29,9 @@ private:
     if (n <= 1) {
       return false;
     }
+    if (n % 2 == 0) return n == 2;
     
-    for (int i = 2; i * i <= n; i++) {
+    for (int i = 3; i * i <= n; i += 2) {
       if (n % i == 0) {
         return false;
       }
@@ -26,7 +43,7 @@ private:
   bool isPalindrome(int n) {
 
     //edge case
-    if (n < 0 || (n % 10 == 0 && n != 0) {
+    if (n < 0 || (n % 10 == 0 && n != 0)) {
       return false;
     }
     int halfRev = 0;
@@ -43,7 +60,7 @@ private:
 
 int main() {
   Solution obj;
-  int n = 5;
+  int n = 13;
   cout << obj.primePalindrome(n);
 
   return 0;
