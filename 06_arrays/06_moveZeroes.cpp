6664ea24ad562cf71f4a118dce5c6194f0a434a3
asitos <4shut0shparashar@gmail.com>
2026-07-22 13:40:37 +0530

    feat(arrays): O(n) two pointers

---
+++ 06_arrays/06_moveZeroes.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+  vector<int> moveZeroes(vector<int>& nums) {
+    vector<int> res;
+    int k;
+     
+     
+     
+    return res;
+  }
+};
+
+int main() {
+  Solution obj;
+  vector<int> nums = {5, 4, 3, 2, 1};
+  vector<int> res = obj.moveZeroes(nums);
+
+  for (auto i : res) {
+    cout << i << " ";
+  }
+  cout << endl;
+
+  return 0;
+}
