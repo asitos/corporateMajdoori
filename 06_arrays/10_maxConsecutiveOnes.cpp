b6a342d221a815657c53d4ae322d18079220e830
asitos <4shut0shparashar@gmail.com>
2026-07-22 17:24:53 +0530

    feat(arrays): O(n) consecutive ones

---
+++ 06_arrays/10_maxConsecutiveOnes.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+  int maxConsecutiveOnes(vector<int>& nums) {
+    int maxOnes = 0;
+    int currentStreak = 0;
+
+    for (int i = 0; i < nums.size(); i++) {
+      if (nums[i] == 1) {
+        currentStreak++;
+        maxOnes = max(maxOnes, currentStreak);
+      } else {
+        currentStreak = 0;
+      }
+    }
+    
+    return maxOnes;
+  }
+};
+
+int main() {
+  Solution obj;
+  vector<int> nums = {1, 1, 0, 1, 1, 1};
+  int res = obj.maxConsecutiveOnes(nums);
+
+  cout << res;
+
+  return 0;
+}
