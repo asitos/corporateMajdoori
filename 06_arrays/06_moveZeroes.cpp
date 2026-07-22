77182727ad84b8406e94351c91936b71b8aacd8d
asitos <4shut0shparashar@gmail.com>
2026-07-22 14:30:57 +0530

    feat(arrays): O(n) anchor and scout two pointers

--- 06_arrays/06_moveZeroes.cpp
+++ 06_arrays/06_moveZeroes.cpp
@@ -3,22 +3,32 @@ using namespace std;
 
 class Solution {
 public:
-  vector<int> moveZeroes(vector<int>& nums) {
-    vector<int> res;
-    int k;
+  void moveZeroes(vector<int>& nums) {
+    // edge case
+    int n = nums.size();
+    if (n <= 1) return;
      
-     
-     
-    return res;
+    int j = 0;
+
+    for (int i = 0; i < n; i++) {
+      if (nums[i] != 0) {
+        nums[j] = nums[i];
+        j++;
+      }
+    }
+
+    for (int i = j; i < n; i++) {
+       nums[i] = 0;
+    }
   }
 };
 
 int main() {
   Solution obj;
-  vector<int> nums = {5, 4, 3, 2, 1};
-  vector<int> res = obj.moveZeroes(nums);
+  vector<int> nums = {1, 0, 2, 3, 2, 0, 0, 4, 5, 1};
+  obj.moveZeroes(nums);
 
-  for (auto i : res) {
+  for (auto i : nums) {
     cout << i << " ";
   }
   cout << endl;
