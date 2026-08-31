483fa2618acc1266352b506815e46d206f81796e
asitos <4shut0shparashar@gmail.com>
2026-08-31 13:07:21 +0530

    feat(arrays): O(n) kadane's algorithm for maximum subarray sum

---
+++ 06_arrays/18_maximumSubarraySum.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+  long long maximumSubarraySum(vector<int> &nums) {
+    int n = nums.size();
+    if (n == 0)
+      return 0;
+
+    long long maxi = LLONG_MIN;
+    long long sum = 0;
+
+    for (int i = 0; i < n; i++) {
+      sum += nums[i];
+
+      if (sum > maxi) {
+        maxi = sum;
+      }
+
+      if (sum < 0) {
+        sum = 0;
+      }
+    }
+
+    return maxi;
+  }
+};
+
+int main() {
+  Solution obj;
+  vector<int> nums = {2, 3, 5, -2, 7, -4};
+
+  long long res = obj.maximumSubarraySum(nums);
+
+  cout << res;
+
+  return 0;
+}
