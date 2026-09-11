4bec62ddb653d60c1a80d336d9741176df3fa4c9
asitos <4shut0shparashar@gmail.com>
2026-09-11 16:32:38 +0530

    feat(gfg): O(n*sum) time, tabulation dp approach for counting ways to partition array into two subsets with minimum difference of their sum

---
+++ gfg/countSubsetWithDifference.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int dp[50][50];
+
+class Solution {
+public:
+  int countPartitions(vector<int> &nums, int diff) {
+    int n = nums.size();
+
+    int totalSum = 0;
+    for (int x : nums) {
+      totalSum += x;
+    }
+
+    // edge case check
+    if (totalSum < diff || (totalSum + diff) % 2 != 0) {
+      return 0;
+    }
+
+    int target = (totalSum + diff) / 2;
+
+    return countSubsetWithSum(nums, target, n);
+  }
+
+  int countSubsetWithSum(vector<int> &nums, int sum, int n) {
+    // initialise table
+    vector<vector<int>> dp(n + 1, vector<int>(sum + 1, 0));
+    dp[0][0] = 1; // 0 sum is always possible
+    for (int j = 1; j <= sum; j++) {
+      dp[0][j] = 0; // 0 elements cannot form any sum > 0
+    }
+
+    // choice
+    for (int i = 1; i <= n; i++) {
+      for (int j = 0; j <= sum; j++) {
+        if (nums[i - 1] <= j) {
+          dp[i][j] = dp[i - 1][j] + dp[i - 1][j - nums[i - 1]];
+        } else {
+          dp[i][j] = dp[i - 1][j];
+        }
+      }
+    }
+    return dp[n][sum];
+  }
+};
+
+int main() {
+  Solution obj;
+  vector<int> nums = {1, 1, 2, 3};
+  int diff = 1;
+  int res = obj.countPartitions(nums, diff);
+
+  cout << res;
+
+  return 0;
+}
